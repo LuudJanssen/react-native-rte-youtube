4033b10e379cc5200cba2d691f7067dd029aec00
Bastiaan <b.m.mevius@student.tue.nl>
2019-02-12 12:33:58 +0100

    init repo

---
+++ App.js
@@ -0,0 +1,82 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
+
+import React, {Component} from 'react';
+import {ScrollView, Platform, StyleSheet, Text, View} from 'react-native';
+import Markdown from 'react-native-simple-markdown';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+        <ScrollView>
+          <Text>test</Text>
+          <Markdown styles={markdownStyles} style={{width: 500, height: 1000}}>
+            #Markdown in react-native is so cool!
+
+            You can **emphasize** what you want, or just _suggest it_ 😏…
+
+            You can even [**link your website**](https://twitter.com/Charles_Mangwa) or if you prefer: [email somebody](mailto:email@somebody.com)
+
+            Spice it up with some GIFs 💃:
+
+            ![Some GIF](https://media.giphy.com/media/dkGhBWE3SyzXW/giphy.gif)
+
+            And even add a cool video 😎!
+
+            [![A cool video from YT](https://img.youtube.com/vi/dQw4w9WgXcQ/0.jpg)](http://www.youtube.com/watch?v=dQw4w9WgXcQ)
+
+            [![Another one from Vimeo](https://i.vimeocdn.com/video/399486266_640.jpg)](https://vimeo.com/57580368)
+          </Markdown>
+        </ScrollView>
+    );
+  }
+}
+
+const markdownStyles = {
+  heading1: {
+    fontSize: 24,
+    color: 'purple',
+  },
+  link: {
+    color: 'pink',
+  },
+  mailTo: {
+    color: 'orange',
+  },
+  text: {
+    color: '#555555',
+  },
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
