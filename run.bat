5090b1136cd45d5075140e956990b3324aadf6ba
Roy van Dijk <r.e.l.v.dijk@student.tue.nl>
2019-02-13 11:09:27 +0100

    Added run.bat & updated package.json

---
+++ run.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+CD /D %~dp0
+:go
+REM cls
+xcopy ..\react-native-simple-markdown .\node_modules\react-native-simple-markdown /E /I /H /R /Y
+react-native run-android
+timeout /t 60
+goto go
