f09ea254a5d287612757a24814247b0cf7ee53e7
Roy van Dijk <r.e.l.v.dijk@student.tue.nl>
2019-02-18 15:54:44 +0100

    Added image picker

--- quillEditor/RichTextEditor.js
+++ quillEditor/RichTextEditor.js
@@ -59,24 +59,20 @@ export default class RichTextEditor extends Component<Props> {
   }
 
   pickImage() {
-    // ImagePicker.showImagePicker(null, (response) => {
-    //   console.log('Response = ', response);
-    //
-    //   if (response.didCancel) {
-    //     console.log('User cancelled image picker');
-    //   } else if (response.error) {
-    //     console.log('ImagePicker Error: ', response.error);
-    //   } else if (response.customButton) {
-    //     console.log('User tapped custom button: ', response.customButton);
-    //   } else {
-    //     const source = { uri: `data:image/jpeg;base64,${response.data}` };
-    //
-    //     // window.postMessage(source);
-    //
-    //     console.log(source);
-    //   }
-    // });
-    this.sendMessage('quill#insertEmbed', [10, 'image', 'someImage']);
+    ImagePicker.showImagePicker({}, (response) => {
+      console.log('Response = ', response);
+
+      if (response.didCancel) {
+        this.log('User cancelled image picker');
+      } else if (response.error) {
+        this.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        this.log('User tapped custom button: ', response.customButton);
+      } else {
+        const source = `data:image/jpeg;base64,${response.data}`;
+        this.sendMessage('quill#insertEmbed', [10, 'image', source]);
+      }
+    });
   }
 
   saveContents(data) {
