1110f3b31e0f07bd20d1ddfcac23696f6e48f8f8
Roy van Dijk <r.e.l.v.dijk@student.tue.nl>
2019-02-19 16:48:09 +0100

    Fixed dimensions and image insert position

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 /**
- * Sample React Native App
+ * Test App for RichTextWrapper
  * https://github.com/facebook/react-native
  *
  * @format
@@ -7,57 +7,58 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, View } from 'react-native';
-import RichTextWrapper from "./quillEditor/RichTextWrapper";
+import React, { Component } from "react";
+import { Platform, StyleSheet, View } from "react-native";
+import RichTextWrapper from "./quillEditor/RichTextWrapper"; // this should become a npm package
 
 const instructions = Platform.select({
-    ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-    android:
-        'Double tap R on your keyboard to reload,\n' +
-        'Shake or press menu button for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  android:
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 type Props = {};
 export default class App extends Component<Props> {
-    render() {
-        return (
-            <View style={{ ...StyleSheet.absoluteFill }}>
-                <RichTextWrapper
-                    onContentsChange={contents => {
-                        console.log('we can set this as state', contents);
-                    }}
-                    config={{
-                        modules: {
-                            //toolbar: false,
-                            toolbar: [
-                                [{ header: [1, 2, false] }],
-                                ['bold'], ['italic'], ['underline'],
-                                ['image'], ['video']
-                            ]
-                        },
-                        theme: 'snow', // or 'bubble'
-                        placeholder: 'Placeholder!',
-                        //readOnly: true,
-                    }}
-                    style={{
-                        //width: '50px',
-                        //height: '50px',
-                    }}
-                    contents={{
-                        ops: [{
-                            insert: 'fgfasdjhs jhksdfjhj',
-                            attributes: {
-                                bold: true
-                            }
-                        }]
-                    }}
-                    imagePickerConfig={{ //https://github.com/react-native-community/react-native-image-picker/blob/master/docs/Reference.md#options
-                        title: 'Kies een afbeelding',
-                        allowsEditing: true,
-                    }}
-                />
-            </View>
-        );
-    }
+  render() {
+    return (
+      <RichTextWrapper
+        onContentsChange={contents => {
+          console.log("we can set this as state", contents);
+        }}
+        config={{
+          modules: {
+            //toolbar: false,
+            toolbar: [
+              [{ header: [1, 2, false] }],
+              ["bold", "italic", "underline"],
+              ["image", "video"]
+            ]
+          },
+          theme: "snow", // or 'bubble'
+          placeholder: "Placeholder!"
+          //readOnly: true,
+        }}
+        style={{
+          width: 300,
+          height: 200
+        }}
+        contents={{
+          ops: [
+            {
+              insert: "fgfasdjhs jhksdfjhj",
+              attributes: {
+                bold: true
+              }
+            }
+          ]
+        }}
+        imagePickerConfig={{
+          //https://github.com/react-native-community/react-native-image-picker/blob/master/docs/Reference.md#options
+          title: "Kies een afbeelding",
+          allowsEditing: true
+        }}
+      />
+    );
+  }
 }
--- quillEditor/RichTextWrapper.js
+++ quillEditor/RichTextWrapper.js
@@ -1,127 +1,125 @@
-import React, { Component } from 'react';
-import { ActivityIndicator, View, WebView } from 'react-native';
-import loadLocalResource from 'react-native-local-resource';
-import Page from './resources/rte.html';
-import CSS from './resources/css.html';
-import JS from './resources/js.html';
+import React, { Component } from "react";
+import { StyleSheet, ActivityIndicator, View, WebView } from "react-native";
+import loadLocalResource from "react-native-local-resource";
+import Page from "./resources/rte.html";
+import CSS from "./resources/css.html";
+import JS from "./resources/js.html";
 import ImagePicker from "react-native-image-picker";
 
 type Props = {};
 export default class RichTextWrapper extends Component<Props> {
-    constructor(props) {
-        super(props);
-        this.webView = React.createRef();
-        this.state = {
-            data: null,
-            rteContent: this.props.contents,
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.webView = React.createRef();
+    this.state = {
+      html: null,
+      rteContent: this.props.contents
+    };
+  }
 
-    sendMessage(name, data) {
-        const msg = { name, data };
-        this.webView.current.postMessage(JSON.stringify(msg));
-    }
+  sendMessage(name, data) {
+    const msg = { name, data };
+    this.webView.current.postMessage(JSON.stringify(msg));
+  }
 
-    receiveMessage(event, callback) {
-        let msg = { name: '', data: event.nativeEvent.data }
-        try {
-            msg = JSON.parse(event.nativeEvent.data);
-        } catch (err) {
-            console.log(err);
+  receiveMessage(event, callback) {
+    let msg = { name: "", data: event.nativeEvent.data };
+    try {
+      msg = JSON.parse(event.nativeEvent.data);
+    } catch (err) {
+      console.log(err);
+    }
+    callback(msg.name, msg.data);
+  }
+
+  log(...params) {
+    console.log(...params);
+  }
+
+  saveContents(data) {
+    this.setState({
+      rteContent: data
+    });
+  }
+
+  onContentsChange(content) {
+    this.props.onContentsChange(content);
+  }
+
+  setStyles() {
+    this.sendMessage("setStyles", style);
+  }
+
+  pickImage() {
+    ImagePicker.showImagePicker(
+      this.props.imagePickerConfig || {
+        allowsEditing: true
+      },
+      response => {
+        console.log("Response = ", response);
+
+        if (response.didCancel) {
+          this.log("User cancelled image picker");
+        } else if (response.error) {
+          this.log("ImagePicker Error: ", response.error);
+        } else if (response.customButton) {
+          this.log("User tapped custom button: ", response.customButton);
+        } else {
+          const source = `data:image/jpeg;base64,${response.data}`;
+          this.sendMessage("insertImage", source);
         }
-        callback(msg.name, msg.data);
+      }
+    );
+  }
+
+  componentDidMount() {
+    Promise.all([
+      loadLocalResource(Page),
+      loadLocalResource(CSS),
+      loadLocalResource(JS)
+    ]).then(resources => {
+      this.setState({
+        html: resources[0]
+          .replace('"QUILLCONFIG"', JSON.stringify(this.props.config))
+          .replace('"QUILLCONTENTS"', JSON.stringify(this.props.contents || {}))
+          .replace("<CSS></CSS>", resources[1])
+          .replace("<JS></JS>", resources[2])
+      });
+    });
+  }
+
+  render() {
+    if (this.state.html === null) {
+      return (
+        <View>
+          <ActivityIndicator size="large" color="blue" />
+        </View>
+      );
     }
 
-    log(...params) {
-        console.log(...params);
-    }
-
-    saveContents(data) {
-        this.setState({
-            rteContent: data,
-        });
-    }
-
-    onContentsChange(content) {
-        this.props.onContentsChange(content);
-    }
-
-    setStyles() {
-        this.sendMessage('setStyles', style);
-    }
-
-    pickImage() {
-        ImagePicker.showImagePicker(this.props.imagePickerConfig || {
-            allowsEditing: true,
-        }, (response) => {
-            console.log('Response = ', response);
-
-            if (response.didCancel) {
-                this.log('User cancelled image picker');
-            } else if (response.error) {
-                this.log('ImagePicker Error: ', response.error);
-            } else if (response.customButton) {
-                this.log('User tapped custom button: ', response.customButton);
-            } else {
-                const source = `data:image/jpeg;base64,${response.data}`;
-                this.sendMessage('quill#insertEmbed', [null, 'image', source]);
-            }
-        });
-    }
-
-    componentDidMount() {
-        Promise.all([loadLocalResource(Page), loadLocalResource(CSS), loadLocalResource(JS)])
-            .then((resources) => {
-                this.setState({
-                    data: resources[0].replace('"QUILLCONFIG"', JSON.stringify(this.props.config))
-                        .replace('"QUILLCONTENTS"', JSON.stringify(this.props.contents || {}))
-                        .replace("QUILLSTYLE", this.convertStyle(this.props.style) || "width: 100%; height: 80vh;")
-                        .replace('<CSS></CSS>', resources[1])
-                        .replace('<JS></JS>', resources[2])
-                });
-            });
-    }
-
-    render() {
-        if (this.state.data === null) {
-            return (
-                <View>
-                    <ActivityIndicator size="large" color="blue" />
-                </View>
-            );
-        }
-
-        return (
-            <WebView
-                ref={this.webView}
-                onMessage={ev => this.receiveMessage(ev, (name, data) => {
-                    this.log(name, data);
-                    if (name.startsWith('app#')) {
-                        const fnName = name.split('app#')[1];
-                        this.log(fnName, ...data);
-                        this[fnName](...data);
-                    }
-                })}
-                javaScriptEnabled
-                domStorageEnabled
-                allowFileAccess
-                onError={ev => console.log(ev)}
-                source={{ html: this.state.data }}
-            />
-        );
-    }
-
-    convertStyle(style) {
-        if (!style) {
-            return;
-        }
-
-        let styleString = '';
-
-        for (let propName in style) {
-            styleString += propName + ': ' + style[propName] + '; ';
-        }
-
-        return styleString;
-    }
+    return (
+      <View style={this.props.style}>
+        <WebView
+          ref={this.webView}
+          style={{ flex: 1, backgroundColor: "rgba(0, 0, 0, 0)" }}
+          onMessage={ev =>
+            this.receiveMessage(ev, (name, data) => {
+              this.log(name, data);
+              if (name.startsWith("app#")) {
+                const fnName = name.split("app#")[1];
+                this.log(fnName, ...data);
+                this[fnName](...data);
+              }
+            })
+          }
+          javaScriptEnabled
+          domStorageEnabled
+          allowFileAccess
+          scalesPageToFit
+          onError={ev => console.log(ev)}
+          source={{ html: this.state.html }}
+        />
+      </View>
+    );
+  }
 }
